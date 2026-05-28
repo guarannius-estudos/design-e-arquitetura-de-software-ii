77d4508a89e702cea83c3b2fbf7bad3bbe812e91
unknown <gusta.antonius@gmail.com>
2026-05-28 18:50:18 -0300

    Teste de biblioteca para conectar ao banco de dados.

--- src/triggers/extract_categoria_produto_sqlalchemy.py
+++ src/triggers/extract_categoria_produto_sqlalchemy.py
@@ -1,14 +1,15 @@
 import azure.functions as func
 import logging
 import os
-import pyodbc
 import time
 import statistics
+
+from sqlalchemy import create_engine, text
 
 app = func.Blueprint()
 
 @app.timer_trigger(schedule="0 0 6 * * *", arg_name="timer", run_on_startup=False)
-def extract_categoria_produto(timer: func.TimerRequest) -> None:
+def extract_categoria_produto_sqlalchemy(timer: func.TimerRequest) -> None:
     server = os.getenv("SQL_SERVER_SOURCE")
     database = os.getenv("SQL_DATABASE_SOURCE")
     user = os.getenv("SQL_USER_SOURCE")
@@ -17,38 +18,33 @@ def extract_categoria_produto(timer: func.TimerRequest) -> None:
     logging.info(f"Server: {server}, Database: {database}, User: {user}, Password: {password}")
 
     conn_str = (
-        "DRIVER={ODBC Driver 18 for SQL Server};"
-        f"SERVER={server};"
-        f"DATABASE={database};"
-        f"UID={user};"
-        f"PWD={password};"
-        "Encrypt=yes;"
-        "TrustServerCertificate=no;"
-        "Connection Timeout=30;"
+        f"mssql+pyodbc://{user}:{password}@{server}/{database}"
+        "?driver=ODBC+Driver+18+for+SQL+Server"
     )
 
     tempos = []
 
     try:
+        engine = create_engine(conn_str)
+
         for i in range(2):
             inicio = time.perf_counter()
 
-            with pyodbc.connect(conn_str) as conn:
-                cursor = conn.cursor()
-                query = "SELECT * FROM erp.categoria_produto"
-                cursor.execute(query)
-                rows = cursor.fetchall()
+            with engine.connect() as conn:
+                query = text("SELECT * FROM erp.categoria_produto")
+                result = conn.execute(query)
+                rows = result.fetchall()
 
             fim = time.perf_counter()
             tempo = fim - inicio
             tempos.append(tempo)
 
-            logging.info(f"[pyodbc] Execução {i+1}: {tempo:.4f}s")
-            logging.info(f"[pyodbc] Total linhas: {len(rows)}")
+            logging.info(f"[SQLAlchemy] Execução {i+1}: {tempo:.4f}s")
+            logging.info(f"[SQLAlchemy] Total linhas: {len(rows)}")
 
         media = statistics.mean(tempos)
 
-        logging.info(f"[pyodbc] Média final: {media:.4f}s")
+        logging.info(f"[SQLAlchemy] Média final: {media:.4f}s")
     except Exception as e:
         logging.error(f"Erro ao ler erp.categoria_produto: {str(e)}")
         raise
